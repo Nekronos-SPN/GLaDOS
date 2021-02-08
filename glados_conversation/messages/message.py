e6a47ce27feed97dca49bb06c68541b4090cc8f7
Nekronos <edubuntu2002@gmail.com>
2021-02-08 14:11:24 +0100

    Unfinished

--- glados_conversation/messages/message.py
+++ glados_conversation/messages/message.py
@@ -3,5 +3,7 @@
 Messages class and methods
 """
 class Message:
-    def __init__(self):
-        ...
+    def __init__(self, trigger:str, moods):
+        self.trigger = trigger
+        # self.ID = self.__get_ID()
+        self.answers = {mood: [] for mood in moods}
