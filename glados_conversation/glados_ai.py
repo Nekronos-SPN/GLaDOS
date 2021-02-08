20ee86fb671750a45afbc1c5c4826eb903781edf
Nekronos <edubuntu2002@gmail.com>
2021-02-08 14:10:15 +0100

    Test

--- glados_conversation/glados_ai.py
+++ glados_conversation/glados_ai.py
@@ -3,6 +3,11 @@
 GLaDOS Voice
 """
 from tts import voice
+from status import gen_info
+from messages.message import Message
 
-text = voice.listen()
-voice.talk(text)
+message1 = Message("NONE", gen_info.moods)
+print(message1.answers)
+
+# text = voice.listen()
+# voice.talk(text)
